54255c06299c46a1ab16963efd0673c555b85771
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-03-06 00:12:21 +0100

    Refactor spec.

--- spec/sagrone_scraper/agent_spec.rb
+++ spec/sagrone_scraper/agent_spec.rb
@@ -3,7 +3,9 @@ require 'sagrone_scraper/agent'
 
 RSpec.describe SagroneScraper::Agent do
   describe '.http_client' do
-    it { expect(described_class.http_client).to be_a(Mechanize) }
-    it { expect(described_class.http_client).to respond_to(:get) }
+    subject { described_class.http_client }
+
+    it { should be_a(Mechanize) }
+    it { should respond_to(:get) }
   end
 end
