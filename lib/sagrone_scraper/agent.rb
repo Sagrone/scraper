047fe0367fce5cfd3082bed12a1dfee3c62be312
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-03-06 00:04:50 +0100

    SagroneScraper::Agent.http_client should be a Mechanize.

--- lib/sagrone_scraper/agent.rb
+++ lib/sagrone_scraper/agent.rb
@@ -1,4 +1,11 @@
+require 'mechanize'
+
 module SagroneScraper
   class Agent
+    def self.http_client
+      Mechanize.new do |agent|
+        agent.max_history = 0
+      end
+    end
   end
 end
--- spec/sagrone_scraper/agent_spec.rb
+++ spec/sagrone_scraper/agent_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 require 'sagrone_scraper/agent'
 
 RSpec.describe SagroneScraper::Agent do
+  describe '.http_client' do
+    it { expect(described_class.http_client).to be_a(Mechanize) }
+    it { expect(described_class.http_client).to respond_to(:get) }
+  end
 end
