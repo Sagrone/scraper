22d68ffa9eef029e5ad7a8c6b81cc6b44c9c4145
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-03-06 00:21:48 +0100

    SagroneScraper::Agent has custom user agent alias.

--- lib/sagrone_scraper/agent.rb
+++ lib/sagrone_scraper/agent.rb
@@ -2,8 +2,11 @@ require 'mechanize'
 
 module SagroneScraper
   class Agent
+    AGENT_ALIASES = ["Linux Firefox", "Linux Mozilla", "Mac Firefox", "Mac Mozilla", "Mac Safari", "Windows Chrome", "Windows IE 8", "Windows IE 9", "Windows Mozilla"]
+
     def self.http_client
       Mechanize.new do |agent|
+        agent.user_agent_alias = AGENT_ALIASES.sample
         agent.max_history = 0
       end
     end
--- spec/sagrone_scraper/agent_spec.rb
+++ spec/sagrone_scraper/agent_spec.rb
@@ -2,10 +2,21 @@ require 'spec_helper'
 require 'sagrone_scraper/agent'
 
 RSpec.describe SagroneScraper::Agent do
+  let(:user_agent_aliases) do
+    [ "Linux Firefox", "Linux Mozilla",
+      "Mac Firefox", "Mac Mozilla", "Mac Safari",
+      "Windows Chrome", "Windows IE 8", "Windows IE 9", "Windows Mozilla" ]
+  end
+
+  describe 'AGENT_ALIASES' do
+    it { expect(described_class::AGENT_ALIASES).to eq(user_agent_aliases) }
+  end
+
   describe '.http_client' do
     subject { described_class.http_client }
 
     it { should be_a(Mechanize) }
     it { should respond_to(:get) }
+    it { expect(subject.user_agent).to match /Mozilla\/5\.0/ }
   end
 end
