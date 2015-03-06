f91f85504bdecaede549372088a0f4777a807ceb
Marius Colacioiu <marius.colacioiu@gmail.com>
2015-03-06 22:45:41 +0100

    Spec: create TwitterParser from SagroneScraper::Parser.
    Add webmock page response. Test correct attributes are parsed from page.

--- lib/sagrone_scraper/parser.rb
+++ lib/sagrone_scraper/parser.rb
@@ -14,7 +14,21 @@ module SagroneScraper
     end
 
     def parse_page!
+      self.class.method_names.each do |name|
+        attributes[name] = send(name)
+      end
       nil
+    end
+
+    private
+
+    def self.method_names
+      @method_names ||= []
+    end
+
+    def self.method_added(name)
+      puts "added #{name} to #{self}"
+      method_names.push(name)
     end
   end
 end
--- spec/sagrone_scraper/parser_spec.rb
+++ spec/sagrone_scraper/parser_spec.rb
@@ -24,4 +24,39 @@ RSpec.describe SagroneScraper::Parser do
       it { expect(parser.attributes).to be_empty }
     end
   end
+
+  describe 'create custom TwitterParser from SagroneScraper::Parser' do
+    class TwitterParser < SagroneScraper::Parser
+      def bio
+        page.at('.ProfileHeaderCard-bio').text
+      end
+
+      def location
+        page.at('.ProfileHeaderCard-locationText').text
+      end
+    end
+
+    before do
+      stub_request_for('https://twitter.com/Milano_JS', 'twitter.com:Milano_JS')
+    end
+
+    let(:page) { Mechanize.new.get('https://twitter.com/Milano_JS') }
+    let(:twitter_parser) { TwitterParser.new(page: page) }
+
+    describe 'should be able to parse page without errors' do
+      it { expect { twitter_parser.parse_page! }.to_not raise_error }
+    end
+
+    it 'should have attributes present after parsing' do
+      expected_attributes = {
+        bio: "Javascript User Group Milano #milanojs",
+        location: "Milan, Italy"
+      }
+
+      twitter_parser.parse_page!
+
+      expect(twitter_parser.attributes).to_not be_empty
+      expect(twitter_parser.attributes).to eq expected_attributes
+    end
+  end
 end
